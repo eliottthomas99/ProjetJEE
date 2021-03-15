9319dbcbfbfd502a8481e4005ef400d00427e373
ki <commit>
2021-03-15 16:56:01 +0100

    v 3.10
    hashage des mdp lors de la creation de compte + stockage du salt

--- src/org/jee/Visiteur.java
+++ src/org/jee/Visiteur.java
@@ -1,9 +1,15 @@
 package org.jee;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 
 public class Visiteur {
 
@@ -43,7 +49,7 @@ public class Visiteur {
 
 	public static returnStatement CreerCompte(MainServlet.civilite civilite, String nom, String prenom, String email, String password,
 			String confirmPassword, String naissance, String addr_rue, String addr_complement, int addr_code_postal,
-			String ville, String pays, MainServlet.preference preference) {
+			String ville, String pays, MainServlet.preference preference) throws NoSuchAlgorithmException, InvalidKeySpecException {
 
 		// 1) Vérifier que tous les champs sont remplis
 
@@ -79,12 +85,28 @@ public class Visiteur {
 				
 				
 				try (Statement stmt = connexion.createStatement()) {
+					
+					/* hachage du passwd */
+					
+					// generation d un salt
+					SecureRandom random = new SecureRandom();
+					byte[] salt = new byte[16];
+					random.nextBytes(salt);
+					
+					//
+					KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+					SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+					
+					// hachage
+					byte[] hash = factory.generateSecret(spec).getEncoded();
+					
+					password = hash.toString();
 
 					String insert_query = String.format(
-							"INSERT INTO membres(civilite,nom,prenom,email,password,naissance,addr_rue"
+							"INSERT INTO membres(civilite,nom,prenom,email,password, salt, naissance,addr_rue"
 									+ ",addr_complement,addr_code_postal,addr_ville,addr_pays,preference,bloque,tentatives,temps) "
-									+ "VALUES ( '%s', '%s','%s', '%s', '%s','%s', '%s', '%s','%s', '%s', '%s','%s','%s','%s','%s');",
-							civilite.ordinal(), nom, prenom, email, password, naissance, addr_rue, addr_complement,
+									+ "VALUES ( '%s', '%s','%s', '%s', '%s', '%s','%s', '%s', '%s','%s', '%s', '%s','%s','%s','%s','%s');",
+							civilite.ordinal(), nom, prenom, email, password, salt, naissance, addr_rue, addr_complement,
 							addr_code_postal, ville, pays, preference.ordinal(),0,0,0);
 					
 					System.out.println(insert_query);
