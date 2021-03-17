8a7a84fbb7100008b401d092b4e020303325c92b
ki <commit>
2021-03-17 18:25:18 +0100

    v 3.27
    clean up de VisiteurServlet

--- src/org/jee/VisiteurServlet.java
+++ src/org/jee/VisiteurServlet.java
@@ -16,38 +16,39 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/Visiteur")
 public class VisiteurServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public VisiteurServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		String pageName = "/visiteur.jsp";
-
-		this.getServletContext().getRequestDispatcher( pageName ).forward( request, response );
+	public VisiteurServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		
-		
-			doGet(request, response);
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
 
-		
+		String pageName = "/visiteur.jsp";
+
+		this.getServletContext().getRequestDispatcher(pageName).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		doGet(request, response);
+
 	}
 
 }
