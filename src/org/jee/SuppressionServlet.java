ea6d2f40b4842ae4be014dfce8f07720f0ad9c3e
ki <commit>
2021-03-17 18:30:33 +0100

    v 3.28
    clean up de Visiteur

--- src/org/jee/SuppressionServlet.java
+++ src/org/jee/SuppressionServlet.java
@@ -18,59 +18,59 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/suppression")
 public class SuppressionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SuppressionServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		String pageName = "/suppressionCompte.jsp";
-
-		this.getServletContext().getRequestDispatcher( pageName ).forward( request, response );
+	public SuppressionServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		// on recupere l email du client
-		 HttpSession maSession = request.getSession();
-		 Membre membre = (Membre)maSession.getAttribute("membre");
-		 String emailMembre = membre.getEmail();
-		 
-		 // connection BDD
-		 Connection connexion = DBManager.getInstance().getConnection();
-		 try (Statement stmt = connexion.createStatement()) {
+		String pageName = "/suppressionCompte.jsp";
 
-		 
-		 // suppression de la table du membre en question
-				String insert_query = String.format(
-						"DELETE FROM membres "
-								+ "where email='%s';",
-						emailMembre);
-				
-				System.out.println(insert_query);
-				
-				int rs = stmt.executeUpdate(insert_query);
-			} catch (SQLException e) {
-				
-				System.out.println(e);
-			}
-		 
+		this.getServletContext().getRequestDispatcher(pageName).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		// on recupere l email du client
+		HttpSession maSession = request.getSession();
+		Membre membre = (Membre) maSession.getAttribute("membre");
+		String emailMembre = membre.getEmail();
+
+		// connection BDD
+		Connection connexion = DBManager.getInstance().getConnection();
+		try (Statement stmt = connexion.createStatement()) {
+
+			// suppression de la table du membre en question
+			String insert_query = String.format("DELETE FROM membres " + "where email='%s';", emailMembre);
+
+			System.out.println(insert_query);
+
+			int rs = stmt.executeUpdate(insert_query);
+		} catch (SQLException e) {
+
+			System.out.println(e);
+		}
+
 		maSession.setAttribute("membre", null);
-		
+
 		String pageName = "/accueil.jsp";
-		this.getServletContext().getRequestDispatcher( pageName ).forward( request, response );
+		this.getServletContext().getRequestDispatcher(pageName).forward(request, response);
 	}
 
 }
